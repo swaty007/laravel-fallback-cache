d238c28eb76b00160dc77a40743add9b630d643a
bennyruso <bennyrusso0007@gmail.com>
2025-02-06 22:05:17 +0200

    v1.0.1 dispatcher events add

--- src/FallbackCacheManager.php
+++ src/FallbackCacheManager.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace Swaty\CacheFallback;
 
 use Illuminate\Cache\CacheManager;
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 
 class FallbackCacheManager extends CacheManager
 {
     public function store($name = null)
     {
-        return new FallbackCacheRepository(parent::store($name)->getStore(), parent::store(config('cache.fallback', 'array'))->getStore());
+        $repository = new FallbackCacheRepository(parent::store($name)->getStore(), parent::store(config('cache.fallback', 'array'))->getStore());
+        if ($this->app->bound(DispatcherContract::class)) {
+            $repository->setEventDispatcher($this->app[DispatcherContract::class]);
+        }
+        return $repository;
     }
 }
